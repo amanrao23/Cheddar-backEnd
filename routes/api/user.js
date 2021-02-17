67d3c9568498acaf4e675e16f132b9381923c210
Aman Rao <aman.rao@hyperverge.co>
2021-02-17 16:56:26 +0530

    Add Register route

---
+++ routes/api/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userController= require('../controllers/user')
+
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+    '/',userController.registerUser
+  );
+  
+  module.exports = router;
