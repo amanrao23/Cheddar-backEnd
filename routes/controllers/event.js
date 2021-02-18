8b6383dd637d3ceba1ddfa2f722f85d55ac4e27a
Aman Rao <aman.rao@hyperverge.co>
2021-02-18 19:18:16 +0530

    Add new event and get events

---
+++ routes/controllers/event.js
@@ -0,0 +1,87 @@
+
+const User = require('../../models/User');
+const Conversation=require('../../models/Conversation')
+const Event=require('../../models/Event')
+
+exports.getEvents = async (req,res)=>{
+      
+    const { chatRoomId,timestamp } = req.body;
+    try {
+        if(!timestamp)
+        {
+        let events=  await Event.aggregate([
+
+            {
+                $match:{
+                 chatRoomId:chatRoomId
+            }},
+            
+             {$sort: {date: 1}} ,
+                {
+                  $group:
+                    {
+                        _id:"$messageId",
+                      sender:{$last: "$sender"},
+                      text:{$last: "$text"},
+                      type:{$last:"$type"},
+                      time:{$last:"$date"}
+                    }
+                }
+              
+      ])
+       
+           res.status(200).send(events)
+        }
+
+        else{
+            let events=  await Event.aggregate([
+
+                {
+                    $match:{
+                    date: { $gt: new Date(timestamp) },
+                     chatRoomId:chatRoomId
+                }},
+                
+                 {$sort: {date: 1}} ,
+                    {
+                      $group:
+                        {
+                            _id:"$messageId",
+                          sender:{$last: "$sender"},
+                          text:{$last: "$text"},
+                          type:{$last:"$type"},
+                          time:{$last:"$date"}
+                        }
+                    }
+                  
+          ])
+           
+               res.status(200).send(events)
+        }
+   
+   
+    } catch (error) {
+        
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+
+  }
+
+
+  exports.newEvent = async (req,res)=>{
+      
+    const { chatRoomId,type,messageId,text } = req.body;
+    try {
+
+    let event=  new Event({ sender:req.user.id, type,messageId, text, chatRoomId })
+        await event.save()
+       res.status(200).send(event)
+   
+    } catch (error) {
+        
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+
+  }
