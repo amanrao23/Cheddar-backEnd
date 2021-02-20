e829538fca6608e6a5e4b7af7ea3af7567f24220
Aman Rao <aman.rao@hyperverge.co>
2021-02-20 18:59:02 +0530

    Add chat socket

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-const cors = require('cors');
-const http = require('http');
-const socketio=require('socket.io')
+const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
 connectDB();
-
+const socketToken = require("./socketToken.json");
 app.use(express.json());
 
 app.use(cors());
@@ -15,13 +15,16 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  // either with send()
+  console.log(socket.id);
 
-io.on('connection', socket => {
-    // either with send()
-    console.log("AAAAAAAAA")
-    
-    });
-    
+  socket.on("join", ({ username }) => {
+    socketToken[username] = socket.id;
+    console.log(socketToken);
+  });
+});
+
 // app.use(function (req, res, next) {
 //     req.io = io;
 //     next();
@@ -30,11 +33,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 
 app.use("/api/user", require("./routes/api/user"));
 
-
 app.use("/api/event", require("./routes/api/event"));
 
-
 server.listen(PORT, () => console.log(`SERVER STARTED ON 5000`));
-
-
-
